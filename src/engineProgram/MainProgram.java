5c8407dfe6c471e68508850d69f51cce94f49aff
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-04-26 16:49:07 -0300

    Dont know whats happening anymore

--- src/engineProgram/MainProgram.java
+++ src/engineProgram/MainProgram.java
@@ -3,13 +3,34 @@ package engineProgram;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
 
-        ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
 
-        UI.printBoard (chessMatch.getPieces());
+
+
+        ChessMatch chessMatch = new ChessMatch();
+        while( true){
+
+            UI.printBoard (chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source:  ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target:  ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+        }
+
 
     }
 }
